120097f64fb937d22b538e05894e23543847e416
franciszek-szeptycki <franciszek.szeptycki@gmail.com>
2024-11-23 22:11:48 +0100

    json output fix

--- presenters/json.go
+++ presenters/json.go
@@ -3,23 +3,24 @@ package presenters
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 	"who-ate-my-space/utils"
 )
 
 func PresentAsJson(dirInfos []utils.DirInfo) {
-	jsonFormattedDirData := make([]map[string]interface{}, 0)
+	formattedDirData := make([]map[string]interface{}, 0)
 	for _, dirInfo := range dirInfos {
 		jsonDirInfo := map[string]interface{}{
 			"absolute_path": dirInfo.AbsolutePath,
 			"size":          dirInfo.Size,
 		}
-		jsonFormattedDirData = append(jsonFormattedDirData, jsonDirInfo)
+		formattedDirData = append(formattedDirData, jsonDirInfo)
 	}
 
-	jsonOutput, err := json.Marshal(jsonFormattedDirData)
+	jsonOutput, err := json.MarshalIndent(formattedDirData, "", "  ")
 	if err != nil {
 		fmt.Printf("error: %v", err)
 		return
 	}
-	fmt.Println(string(jsonOutput))
+	os.Stdout.Write(jsonOutput)
 }
